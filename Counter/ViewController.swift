0c7d3cd855ec14e01de8c2577c94cc1d5d8e059b
escapist <62696991+escapist132@users.noreply.github.com>
2024-12-27 21:43:16 +0300

    added logic and front for complicated task

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,17 +9,73 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet private weak var counterLabel: UILabel!
+    
+    @IBOutlet private weak var logTextView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    private var counter: Int = 0
+    
+    enum logStatusVariants {
+        case plus
+        case minus
+        case reset
+        case zero
     }
     
-    var counter: Int = 0
-
-    @IBAction func buttonDidTap(_ sender: Any) {
+    @IBAction private func minusButton(_ sender: Any) {
+        if counter > 0 {
+            counter -= 1
+            textToCounterLabel()
+            logToTextView(logStatus: .minus)
+        } else {
+            logToTextView(logStatus: .zero)
+        }
+    }
+    @IBAction private func plusButton(_ sender: Any) {
         counter += 1
+        textToCounterLabel()
+        logToTextView(logStatus: .plus)
+    }
+    @IBAction private func resetButton(_ sender: Any) {
+        counter = 0
+        textToCounterLabel()
+        logToTextView(logStatus: .reset)
+    }
+    
+    private func logToTextView(logStatus: logStatusVariants) {
+        switch logStatus {
+        case .plus:
+            logTextView.text += "\n\(currentDate()): значение изменено на +1"
+        case .minus:
+            logTextView.text += "\n\(currentDate()): значение изменено на -1"
+        case .reset:
+            logTextView.text += "\n\(currentDate()): значение сброшено"
+        case .zero:
+            logTextView.text += "\n\(currentDate()): попытка уменьшить значение счётчика ниже 0"
+        }
+        
+//      Прокрутка textView при добавлении новой строки
+        let range = NSRange(location: logTextView.text.count - 1, length: 0)
+        logTextView.scrollRangeToVisible(range)
+    }
+    
+    private func textToCounterLabel() {
         counterLabel.text = "Значение счётчика: \(counter)"
     }
+    
+    private func currentDate() -> String {
+        let date = Date()
+        let calendar = Calendar.current
+        let year = calendar.component(.year, from: date)
+        let month = calendar.component(.month, from: date)
+        let day = calendar.component(.day, from: date)
+        let hour = calendar.component(.hour, from: date)
+        let minute = calendar.component(.minute, from: date)
+        let second = calendar.component(.second, from: date)
+        return "\(day).\(month).\(year) \(hour):\(minute):\(second)"
+    }
+
 }
