6413a48ad14d46abf3ce7571e061efa9c40a2156
escapist <62696991+escapist132@users.noreply.github.com>
2024-12-27 18:21:55 +0300

    Создан проект

---
+++ CounterTests/CounterTests.swift
@@ -0,0 +1,17 @@
+//
+//  CounterTests.swift
+//  CounterTests
+//
+//  Created by MacUser on 12/27/24.
+//
+
+import Testing
+@testable import Counter
+
+struct CounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
